49877b1193498f5d991dbfa579670c5fc3a14c70
JoustEdicts <julien.m.lombardo@gmail.com>
2025-07-05 20:34:28 +0200

    Initial commit

---
+++ RematchStratRouletteWASM/Models/Card.cs
@@ -0,0 +1,9 @@
+﻿namespace RematchStratRouletteWASM.Models
+{
+    public class Card
+    {
+        public string Title { get; set; }
+        public string GifPath { get; set; }
+        public string Rules { get; set; }
+    }
+}
