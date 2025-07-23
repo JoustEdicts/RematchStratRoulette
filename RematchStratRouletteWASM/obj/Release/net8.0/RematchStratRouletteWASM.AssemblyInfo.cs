7d54877c496525d908132a8dc7eb29300cc68e99
JoustEdicts <julien.m.lombardo@gmail.com>
2025-07-23 02:27:00 +0200

    feat: added card 'Traitor'

--- RematchStratRouletteWASM/obj/Release/net8.0/RematchStratRouletteWASM.AssemblyInfo.cs
+++ RematchStratRouletteWASM/obj/Release/net8.0/RematchStratRouletteWASM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RematchStratRouletteWASM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bccd87a1c1660459895bb0867705b708552c39b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a792e06603dbba4bf24cedeb64d0ed6f4f73ebf")]
 [assembly: System.Reflection.AssemblyProductAttribute("RematchStratRouletteWASM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RematchStratRouletteWASM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
