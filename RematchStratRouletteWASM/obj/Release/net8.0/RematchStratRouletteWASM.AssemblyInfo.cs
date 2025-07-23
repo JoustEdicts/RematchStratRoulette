af2cba912c80f41ff48cbe68fc210fc17abd5213
JoustEdicts <julien.m.lombardo@gmail.com>
2025-07-23 02:48:14 +0200

    feat: enable carousel manual scrolling

--- RematchStratRouletteWASM/obj/Release/net8.0/RematchStratRouletteWASM.AssemblyInfo.cs
+++ RematchStratRouletteWASM/obj/Release/net8.0/RematchStratRouletteWASM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RematchStratRouletteWASM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d54877c496525d908132a8dc7eb29300cc68e99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+093b2451a372e17763aa663ce29e2b9bccb9cb0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("RematchStratRouletteWASM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RematchStratRouletteWASM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
